b76455ad1d05759745fdf6107811167f57415f94
ilootthebody <ilootthebody@gmail.com>
2022-12-29 08:57:06 -0600

    NPC generator progress.

--- javascript/npcScripts.js
+++ javascript/npcScripts.js
@@ -1,3 +1,31 @@
-function generateNPC {
+async function generateNPC() {
+    const API_URL = "https://l3ks5hv18d.execute-api.us-east-2.amazonaws.com/dev/generatenpc";
+
+    // Instantiate and populate header.
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    // create a JSON object with parameters for API call and store in a variable
+    var raw = JSON.stringify({
+        "future": "NONE"
+    });
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    // Call API and await response
+    const response = await fetch(API_URL, requestOptions);
+    const responseJSON = await response.json();
+    const npcDetails = JSON.parse(responseJSON.body);
+
+    console.log(npcDetails);
+
+    document.getElementById("npc-name-header").textContent = npcDetails.name;
+    document.getElementById("npc-race-details").textContent = npcDetails.race;
+    document.getElementById("npc-appearance-text").textContent = npcDetails.appearance;
     
+
 }
