be3b42d50cf0bc50cb643dbf500521a79ac8c36c
ilootthebody <ilootthebody@gmail.com>
2023-01-02 12:54:07 -0600

    Added ability to regenerate specific fields of NPC generator.

--- javascript/npcScripts.js
+++ javascript/npcScripts.js
@@ -21,11 +21,9 @@ async function generateNPC() {
     const responseJSON = await response.json();
     const npcDetails = JSON.parse(responseJSON.body);
 
-    console.log(npcDetails);
-
     // Update HTML
     document.getElementById("npc-name-header").textContent = npcDetails.name;
-    document.getElementById("npc-race-details").textContent = npcDetails.race;
+    document.getElementById("npc-race-text").textContent = npcDetails.race;
     document.getElementById("npc-appearance-text").textContent = npcDetails.appearance;
     document.getElementById("npc-clothing-text").textContent = npcDetails.clothing;
     document.getElementById("npc-voice-text").textContent = npcDetails.voice;
@@ -35,7 +33,45 @@ async function generateNPC() {
 }
 
 async function regenField(field) {
-    if (field == "name") {
-        console.log("Regenerating name.")
-    }
+    const API_URL = "https://l3ks5hv18d.execute-api.us-east-2.amazonaws.com/dev/iltbregenfield";
+
+    // Instantiate and populate header.
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    // create a JSON object with parameters for API call and store in a variable
+    var raw = JSON.stringify({
+        "field": field,
+        "oldFields": {
+            "npc-name-header": document.getElementById("npc-name-header").textContent,
+            "npc-race-text": document.getElementById("npc-race-text").textContent,
+            "npc-appearance-text": document.getElementById("npc-appearance-text").textContent,
+            "npc-clothing-text": document.getElementById("npc-clothing-text").textContent,
+            "npc-voice-text": document.getElementById("npc-voice-text").textContent,
+            "npc-personality-text": document.getElementById("npc-personality-text").textContent,
+            "npc-profession-text": document.getElementById("npc-profession-text").textContent,
+            "npc-motivation-text": document.getElementById("npc-motivation-text").textContent
+        }
+    });
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    // Call API and await response
+    const response = await fetch(API_URL, requestOptions);
+    const responseJSON = await response.json();
+    const newFields = JSON.parse(responseJSON.body);
+
+    // Update HTML
+    document.getElementById("npc-name-header").textContent = newFields.name;
+    document.getElementById("npc-race-text").textContent = newFields.race;
+    document.getElementById("npc-appearance-text").textContent = newFields.appearance;
+    document.getElementById("npc-clothing-text").textContent = newFields.clothing;
+    document.getElementById("npc-voice-text").textContent = newFields.voice;
+    document.getElementById("npc-personality-text").textContent = newFields.personality;
+    document.getElementById("npc-profession-text").textContent = newFields.profession;
+    document.getElementById("npc-motivation-text").textContent = newFields.motivation;
 }
