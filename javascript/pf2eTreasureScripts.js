7cbc5a4ba2474b5d3a52d011a2c78bd1e6747501
ilootthebody <ilootthebody@gmail.com>
2023-09-03 17:34:01 -0500

    Updated pf2e treasure generator script to successfully call api and display items to html.

--- javascript/pf2eTreasureScripts.js
+++ javascript/pf2eTreasureScripts.js
@@ -1,44 +1,40 @@
 // -------------------------------------------------------------------------------- \\
 // Gathers relevant information from the checkboxes, send the API request to AWS, 
-// and displays the resulting NPC when the 'Generate NPC' button is pressed.
+// and displays the items when the 'Generate Treasure' button is pressed.
 // -------------------------------------------------------------------------------- \\
-async function generateNPC() {
-    const API_URL = "https://l3ks5hv18d.execute-api.us-east-2.amazonaws.com/dev/generatenpc";
+async function generatePf2eTreasure() {
+    const API_URL = "https://l3ks5hv18d.execute-api.us-east-2.amazonaws.com/dev/pf2e-treasure-generator";
+
+    // Hide error text
+    document.getElementById("error-p").style.display = "none";
 
     // Instantiate and populate header.
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
-    // Grab ancestries from HTML.
-    var races = [];
-    var raceCheckboxes = document.getElementsByName("ancestry-checkbox");
-    for (var i = 0; i < raceCheckboxes.length; i++) {
-        if (raceCheckboxes[i].checked) { races.push(raceCheckboxes[i].value); }
+    // Grab levels from HTML.
+    var levels = [];
+    var levelCheckboxes = document.getElementsByName("level-checkbox");
+    for (var i = 0; i < levelCheckboxes.length; i++) {
+        if (levelCheckboxes[i].checked) { levels.push(levelCheckboxes[i].value); }
     };
-    console.log(races.toString());
 
-    // Grab genders from HTML.
-    var genders = [];
-    var genderCheckboxes = document.getElementsByName("gender-checkbox");
-    for (var i = 0; i < genderCheckboxes.length; i++) {
-        if (genderCheckboxes[i].checked) { genders.push(genderCheckboxes[i].value); }
+    // Grab categories from HTML.
+    var categories = [];
+    var categoryCheckboxes = document.getElementsByName("category-checkbox");
+    for (var i = 0; i < categoryCheckboxes.length; i++) {
+        if (categoryCheckboxes[i].checked) { categories.push(categoryCheckboxes[i].value); }
     };
-    console.log(genders.toString());
 
-    // Grab ages from HTML.
-    var ages = [];
-    var ageCheckboxes = document.getElementsByName("age-checkbox");
-    for (var i = 0; i < ageCheckboxes.length; i++) {
-        if (ageCheckboxes[i].checked) { ages.push(ageCheckboxes[i].value); }
-    };
-    console.log(ages.toString());
+    // Grab number of items from HTML.
+    var i = document.getElementById("num_items");
+    var numItems = i.options[i.selectedIndex].text;
 
     // create a JSON object with parameters for API call and store in a variable
     var raw = JSON.stringify({
-        "races": races.toString(),
-        "genders": genders.toString(),
-        "ages": ages.toString(),
-        "system": "pf2e"
+        "categories": categories.toString(),
+        "levels": levels.toString(),
+        "numItems": numItems
     });
     var requestOptions = {
         method: 'POST',
@@ -47,96 +43,56 @@ async function generateNPC() {
         redirect: 'follow'
     };
 
+    console.log(raw);
+
     // Call API and await response
     const response = await fetch(API_URL, requestOptions);
     const responseJSON = await response.json();
-    const npcDetails = JSON.parse(responseJSON.body);
+    const statusCode = responseJSON.statusCode;
+    const items = JSON.parse(responseJSON.body);
+
+    console.log(items);
 
     // Update HTML
-    document.getElementById("npc-name-header").textContent = npcDetails.name;
-    document.getElementById("npc-race-text").textContent = npcDetails.race;
-    document.getElementById("npc-appearance-text").textContent = npcDetails.appearance;
-    document.getElementById("npc-clothing-text").textContent = npcDetails.clothing;
-    document.getElementById("npc-voice-text").textContent = npcDetails.voice;
-    document.getElementById("npc-personality-text").textContent = npcDetails.personality;
-    document.getElementById("npc-profession-text").textContent = npcDetails.profession;
-    document.getElementById("npc-motivation-text").textContent = npcDetails.motivation;
+    if (statusCode == 500) {
+        document.getElementById("error-p").style.display = "inline-block";
+        document.getElementById("error-p").textContent = items;
+    }
+    else {
+        itemNum = 1;
+        for (var item of items.items) {
+            document.getElementById("item-" + itemNum.toString()).style.display = "list-item";
+            document.getElementById("item-" + itemNum.toString() + "-name").textContent = titleCase(item.name);
+            document.getElementById("item-" + itemNum.toString() + "-cat").textContent = titleCase(item.category);
+            document.getElementById("item-" + itemNum.toString() + "-lvl").textContent = item.level;
+            document.getElementById("item-" + itemNum.toString() + "-name").href = item.link;
+            itemNum += 1;
+        }
+
+        // Hide any unused items
+        for (itemNum; itemNum <= 5; itemNum++) { document.getElementById("item-" + itemNum.toString()).style.display = "none"; }
+    }
+
+
+    // document.getElementById("npc-name-header").textContent = npcDetails.name;
+    // document.getElementById("npc-race-text").textContent = npcDetails.race;
+    // document.getElementById("npc-appearance-text").textContent = npcDetails.appearance;
+    // document.getElementById("npc-clothing-text").textContent = npcDetails.clothing;
+    // document.getElementById("npc-voice-text").textContent = npcDetails.voice;
+    // document.getElementById("npc-personality-text").textContent = npcDetails.personality;
+    // document.getElementById("npc-profession-text").textContent = npcDetails.profession;
+    // document.getElementById("npc-motivation-text").textContent = npcDetails.motivation;
 }
 
 // -------------------------------------------------------------------------------- \\
-// Regenerates a specific field within the NPC details when the refresh button is
-// pressed.
+// Converts a string to title case.
 // -------------------------------------------------------------------------------- \\
-async function regenField(field) {
-    const API_URL = "https://l3ks5hv18d.execute-api.us-east-2.amazonaws.com/dev/iltbregenfield";
-
-    // Instantiate and populate header.
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-
-    // Grab ancestries from HTML.
-    var races = [];
-    var raceCheckboxes = document.getElementsByName("ancestry-checkbox");
-    for (var i = 0; i < raceCheckboxes.length; i++) {
-        if (raceCheckboxes[i].checked) { races.push(raceCheckboxes[i].value); }
-    };
-    console.log(races.toString());
-
-    // Grab genders from HTML.
-    var genders = [];
-    var genderCheckboxes = document.getElementsByName("gender-checkbox");
-    for (var i = 0; i < genderCheckboxes.length; i++) {
-        if (genderCheckboxes[i].checked) { genders.push(genderCheckboxes[i].value); }
-    };
-    console.log(genders.toString());
-
-    // Grab ages from HTML.
-    var ages = [];
-    var ageCheckboxes = document.getElementsByName("age-checkbox");
-    for (var i = 0; i < ageCheckboxes.length; i++) {
-        if (ageCheckboxes[i].checked) { ages.push(ageCheckboxes[i].value); }
-    };
-    console.log(ages.toString());
-
-    // create a JSON object with parameters for API call and store in a variable
-    var raw = JSON.stringify({
-        "field": field,
-        "oldFields": {
-            "npc-name-header": document.getElementById("npc-name-header").textContent,
-            "npc-race-text": document.getElementById("npc-race-text").textContent,
-            "npc-appearance-text": document.getElementById("npc-appearance-text").textContent,
-            "npc-clothing-text": document.getElementById("npc-clothing-text").textContent,
-            "npc-voice-text": document.getElementById("npc-voice-text").textContent,
-            "npc-personality-text": document.getElementById("npc-personality-text").textContent,
-            "npc-profession-text": document.getElementById("npc-profession-text").textContent,
-            "npc-motivation-text": document.getElementById("npc-motivation-text").textContent
-        },
-        "races": races.toString(),
-        "genders": genders.toString(),
-        "ages": ages.toString(),
-        "system": "pf2e"
-    });
-    var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
-
-    // Call API and await response
-    const response = await fetch(API_URL, requestOptions);
-    const responseJSON = await response.json();
-    const newFields = JSON.parse(responseJSON.body);
-
-    // Update HTML
-    document.getElementById("npc-name-header").textContent = newFields.name;
-    document.getElementById("npc-race-text").textContent = newFields.race;
-    document.getElementById("npc-appearance-text").textContent = newFields.appearance;
-    document.getElementById("npc-clothing-text").textContent = newFields.clothing;
-    document.getElementById("npc-voice-text").textContent = newFields.voice;
-    document.getElementById("npc-personality-text").textContent = newFields.personality;
-    document.getElementById("npc-profession-text").textContent = newFields.profession;
-    document.getElementById("npc-motivation-text").textContent = newFields.motivation;
+function titleCase(str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+    return str.join(' ');
 }
 
 // -------------------------------------------------------------------------------- \\
@@ -144,7 +100,7 @@ async function regenField(field) {
 // -------------------------------------------------------------------------------- \\
 function toggleAllLevels(source) {
     levelBoxes = document.querySelectorAll("[name='level-checkbox']");
-    for (var i=0; i < levelBoxes.length; i++) {
+    for (var i = 0; i < levelBoxes.length; i++) {
         levelBoxes[i].checked = source.checked;
     }
 }
@@ -154,7 +110,7 @@ function toggleAllLevels(source) {
 // -------------------------------------------------------------------------------- \\
 function toggleAllCategories(source) {
     categoryBoxes = document.querySelectorAll("[name='category-checkbox']");
-    for (var i=0; i < categoryBoxes.length; i++) {
+    for (var i = 0; i < categoryBoxes.length; i++) {
         categoryBoxes[i].checked = source.checked;
     }
 }
