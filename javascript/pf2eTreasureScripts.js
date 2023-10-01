883f157d934803ed71d90d69076f834b8e975a2a
ilootthebody <ilootthebody@gmail.com>
2023-09-30 19:15:34 -0500

    Added input validation to pf2e loot generator.

--- javascript/pf2eTreasureScripts.js
+++ javascript/pf2eTreasureScripts.js
@@ -1,6 +1,6 @@
 // -------------------------------------------------------------------------------- \\
 // Gathers relevant information from the checkboxes, send the API request to AWS, 
-// and displays the items when the 'Generate Treasure' button is pressed.
+// and displays the items when the 'Generate Loot' button is pressed.
 // -------------------------------------------------------------------------------- \\
 async function generatePf2eTreasure() {
     const API_URL = "https://l3ks5hv18d.execute-api.us-east-2.amazonaws.com/dev/pf2e-treasure-generator";
@@ -12,28 +12,45 @@ async function generatePf2eTreasure() {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
-    // Grab levels from HTML.
+    // Grab levels from HTML
     var levels = [];
     var levelCheckboxes = document.getElementsByName("level-checkbox");
     for (var i = 0; i < levelCheckboxes.length; i++) {
         if (levelCheckboxes[i].checked) { levels.push(levelCheckboxes[i].value); }
     };
+    // Validate that at least one level is selected
+    if (levels.length == 0) {
+        document.getElementById("error-p").textContent = "ERROR: You must select at least one item level.";
+        document.getElementById("error-p").style.display = "inline-block";
+        return;
+    }
 
-    // Grab categories from HTML.
+    // Grab categories from HTML
     var categories = [];
     var categoryCheckboxes = document.getElementsByName("category-checkbox");
     for (var i = 0; i < categoryCheckboxes.length; i++) {
         if (categoryCheckboxes[i].checked) { categories.push(categoryCheckboxes[i].value); }
     };
+    // Validate that at least one category is selected
+    if (categories.length == 0) {
+        document.getElementById("error-p").textContent = "ERROR: You must select at least one item category.";
+        document.getElementById("error-p").style.display = "inline-block";
+        return;
+    }
 
     // Grab number of items from HTML.
     var i = document.getElementById("num-items");
 
     // Validate number of items input
-    if (!i.value) {
-        console.log("No input in number of items.")
+    var betweenOneAndTwenty = /^([1-9]|1[0-9]|20)$/ // Number between 1 and 20
+    if (i.value.match(betweenOneAndTwenty)) {
+        var numItems = i.value;
     }
-    var numItems = i.value;
+    else {
+        document.getElementById("error-p").textContent = "ERROR: Number of items must be a number between 1-20.";
+        document.getElementById("error-p").style.display = "inline-block";
+        return;
+    }
 
     // create a JSON object with parameters for API call and store in a variable
     var raw = JSON.stringify({
@@ -53,27 +70,23 @@ async function generatePf2eTreasure() {
     // Call API and await response
     const response = await fetch(API_URL, requestOptions);
     const responseJSON = await response.json();
-    const statusCode = responseJSON.statusCode;
-    
+
     // Check for uncaught server side error
     if (responseJSON.errorMessage) {
         document.getElementById("error-p").style.display = "inline-block";
-        document.getElementById("error-p").textContent = "Server error. Please try reducing the number of items generated or contact us at ilootthebody@gmail.com.";
+        document.getElementById("error-p").textContent = "Server error. Please try again or contact us at ilootthebody@gmail.com.";
         return;
     }
 
-    // Parse JSON into dict
-    const items = JSON.parse(responseJSON.body);
-
-    console.log(items);
-
     // Display error message if error detected
-    if (statusCode == 500) {
+    if (responseJSON.statusCode == 500) {
         document.getElementById("error-p").style.display = "inline-block";
         document.getElementById("error-p").textContent = items;
     }
     // Update item table with generated items
     else {
+        // Parse JSON into dict
+        const items = JSON.parse(responseJSON.body);
         var itemNum = 1;
 
         // Get body of item table
@@ -98,7 +111,7 @@ async function generatePf2eTreasure() {
             itemCat.innerHTML = titleCase(item.category);
             itemLevel.innerHTML = item.level;
             itemPrice.innerHTML = item.price;
-            
+
             // Create link for name cell
             var a = document.createElement('a');
             var tn = document.createTextNode(titleCase(item.name));
