4cd01d6a42e09c25b211f25d96c22247a6c278cf
ilootthebody <ilootthebody@gmail.com>
2024-03-30 19:44:36 -0500

    Reconfigured DND5e HB item gen to work with additional power level and new suggested rarity.

--- javascript/magicItemScripts.js
+++ javascript/magicItemScripts.js
@@ -109,13 +109,19 @@ async function getItems() {
     // Index for iterating over number of items
     itemNum = 1;
 
-    // Display Generated Items. Item array is formatted as [item_name, effect_position, effect_name, details, effect_desc]
+    // Display Generated Items.
+    // Item array is [item_name, effect_position, effect_name, details, effect_desc]
     for (var item of itemList.items) {
+        console.log(item);
         var itemName = item[0];
         var effectPosition = item[1];
         var effectName = item[2];
         var itemDetails = item[3];
-        var effectDescription = item[4];
+        var attunement = item[4];
+        var itemPrice = item[5];
+        var itemRarity = item[6];
+        var cursed = item[7];
+        var effectDescription = item[8];
 
         document.getElementById("item" + itemNum + "-loader").style.display = "none";
         document.getElementById("item" + itemNum + "-name").textContent = itemName;
@@ -127,6 +133,20 @@ async function getItems() {
         else if (effectPosition == "SUFFIX") {
             document.getElementById("item" + itemNum + "-suffix").textContent = effectName;
             document.getElementById("item" + itemNum + "-prefix").textContent = "";
+        }
+
+        // Format details
+        if (options.includes("rarity")) {
+            itemDetails += ", " + itemRarity;
+        }
+        if (attunement) {
+            itemDetails += ", Requires Attunement";
+        }
+        if (cursed) {
+            itemDetails += ", Cursed";
+        }
+        if (options.includes("price")) {
+            itemDetails += ", Suggested Price: " + itemPrice;
         }
 
         document.getElementById("item" + itemNum + "-det").textContent = itemDetails;
@@ -150,8 +170,11 @@ async function regenEffect(effectID) {
     itemNum = splitID[0];
     effectNum = splitID[1];
 
-    // API URL
-    const API_URL = "https://l3ks5hv18d.execute-api.us-east-2.amazonaws.com/dev/iltbgetitem";
+    // Create API URL. If in DEV, change it.
+    var API_URL = "https://r5v52vn69g.execute-api.us-east-2.amazonaws.com/iltb/PRD-DND5E-Regen-HB-Effect";
+    if (document.location.hostname != "www.ilootthebody.com") {
+        API_URL = "https://qi9d504bkk.execute-api.us-east-2.amazonaws.com/iltb/DEV-DND5E-Regen-HB-Effect";
+    }
 
     // Grab power levels from HTML
     var powerCheckboxes = document.getElementsByName("power-level-checkbox");
@@ -229,22 +252,22 @@ async function regenEffect(effectID) {
 // Displays a generic error message to the user.
 // -------------------------------------------------------------------------------- \\
 function displayErrorMessage() {
-    const itemContainer = document.getElementById("item-container");
+    const itemContainer = document.getElementById("result-container");
 
     // Clear old items from item container.
-    while (itemContainer.firstChild) {
-        itemContainer.removeChild(itemContainer.firstChild);
-    }
-
-    var h2 = document.createElement("h2");
-    var h2Text = document.createTextNode("Oops...");
-    h2.appendChild(h2Text);
-    itemContainer.appendChild(h2);
+    // while (itemContainer.firstChild) {
+    //     itemContainer.removeChild(itemContainer.firstChild);
+    // }
 
     var detailsPara = document.createElement("p");
     var detailsText = document.createTextNode("Looks like we rolled a nat 1 on our investigation check. Please try again later.");
     detailsPara.appendChild(detailsText);
-    itemContainer.appendChild(detailsPara);
+    itemContainer.prepend(detailsPara);
+
+    var h2 = document.createElement("h2");
+    var h2Text = document.createTextNode("Oops...");
+    h2.appendChild(h2Text);
+    itemContainer.prepend(h2);
 
     itemContainer.style.display = "inline-block";
 }
